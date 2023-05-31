187f8229842d5798844eb6d14b7a5d9343b695f5
Tushar Goyal <tgoyal63@gmail.com>
2023-05-31 11:37:04 +0530

    Fixed DB connection string

--- server/utils/db-conn.utils.js
+++ server/utils/db-conn.utils.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose');
 
 const dbHost = process.env.DB_HOST || "localhost";
-const dbPort = process.env.DB_PORT || 27017;
+// const dbPort = process.env.DB_PORT || 27017;
 const dbName = process.env.DB_NAME || "hackoverflow";
-// const dbUser = process.env.DB_USER;
-// const dbNPass = process.env.DB_PASSWORD;
+const dbUser = process.env.DB_USER || "";
+const dbPass = process.env.DB_PASSWORD || "";
 
 const dbConn = {
   connect: () => {
-    const url = `mongodb://${dbHost}:${dbPort}/${dbName}`;
+    const url = `mongodb+srv://${dbUser}:${dbPass}@${dbHost}/${dbName}`;
+
+    // const url = "mongodb+srv://tushar:4O4m3y4anjLSwS7B@formcluster.70ocf.mongodb.net/hackoverflow";
     return mongoose.connect(url, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -17,7 +19,7 @@ const dbConn = {
   safeConnect: async () => {
     try {
       await dbConn.connect();
-      console.log(`Connected to database: ${dbName} at ${dbHost}:${dbPort}`);
+      console.log(`Connected to database: ${dbName} at ${dbHost}`);
     } catch (error) {
       console.log('Connection to database failed. Error: ', error);
       console.log('Closing application');
